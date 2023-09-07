e7d13014d9b42492d3732615deff5c43fa56b594
alirabah93 <110853127+alirabah93@users.noreply.github.com>
2023-09-07 15:46:06 +0300

    make fake data for users

---
+++ react/react_profile/src/data/users.js
@@ -0,0 +1,322 @@
+
+export const profiles = {
+    "@reactjs": {
+        'name': 'React',
+        'description': 'The library for web and native user interfaces',
+        'joinedDate': 'July 2013',
+        'webLink': 'react.dev',
+        'profilePicture': 'https://i0.wp.com/www.primefaces.org/wp-content/uploads/2017/09/feature-react.png?ssl=1',
+        'profileCover': 'https://res.cloudinary.com/practicaldev/image/fetch/s--ffm9Lfjx--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/sc4ung0l605tn9oux9ee.png',
+        'postsNumber': 2611,
+        'posts': [
+            {
+                'id': 1,
+                'addedBy': '@dan_abramov',
+                'dateAdded': 'May 29',
+                'content': 'Happy 10th birthday to @reactjs!',
+                'likes': 3701,
+                'comments': 46,
+                'repost': 675,
+                'reached': '1.1M'
+            },
+        ],
+        'following': {
+            'users': ['@MohamadDewiekat', '@MohamadKukhun', '@YazanAlsade', '@Node.js', '@typescript', '@tailwindcss', '@freeCodeCamp', '@mussaWaleed', '@AhmadHassan', '@dan_abramov'],
+            'number': 267
+        },
+        'followers': {
+            'users': ['@MohamadDewiekat', '@MohamadKukhun', '@YazanAlsade', '@Node.js', '@typescript', '@tailwindcss', '@freeCodeCamp', '@mussaWaleed', '@AhmadHassan', '@dan_abramov'],
+            'number': 7081000
+        }
+    },
+    "@MohamadDewiekat": {
+        'name': 'Mohamad Dewiekat',
+        'description': 'Front-end Developer | JavaScript Enthusiast',
+        'joinedDate': 'September 2015',
+        'webLink': 'mohamaddewiekat.com',
+        'profilePicture': 'hhttps://xsgames.co/randomusers/assets/avatars/male/46.jpg',
+        'profileCover': 'https://example.com/cover/mohamad-dewiekat-cover.png',
+        'postsNumber': 120,
+        'posts': [
+            {
+                'id': 1,
+                'addedBy': '@MohamadDewiekat',
+                'dateAdded': 'August 12',
+                'content': 'Just launched my new portfolio website!',
+                'likes': 210,
+                'comments': 15,
+                'repost': 30,
+                'reached': '50K'
+            },
+        ],
+        'following': {
+            'users': ['@reactjs', '@MohamadKukhun', '@YazanAlsade', '@Node.js', '@typescript', '@tailwindcss', '@freeCodeCamp', '@mussaWaleed', '@AhmadHassan', '@dan_abramov'],
+            'number': 45
+        },
+        'followers': {
+            'users': ['@reactjs', '@MohamadKukhun', '@YazanAlsade', '@Node.js', '@typescript', '@tailwindcss', '@freeCodeCamp', '@mussaWaleed', '@AhmadHassan', '@dan_abramov'],
+            'number': 1234
+        }
+    },
+    "@MohamadKukhun": {
+        'name': 'Mohamad Kukhun',
+        'description': 'Full Stack Developer | Coffee Lover',
+        'joinedDate': 'March 2018',
+        'webLink': 'kukhun.dev',
+        'profilePicture': 'https://xsgames.co/randomusers/assets/avatars/male/74.jpg',
+        'profileCover': 'https://example.com/cover/mohamad-kukhun-cover.png',
+        'postsNumber': 98,
+        'posts': [
+            {
+                'id': 1,
+                'addedBy': '@MohamadKukhun',
+                'dateAdded': 'July 5',
+                'content': 'Enjoying a cup of coffee while coding ☕️',
+                'likes': 150,
+                'comments': 10,
+                'repost': 20,
+                'reached': '30K'
+            },
+        ],
+        'following': {
+            'users': ['@reactjs', '@MohamadDewiekat', '@YazanAlsade', '@Node.js', '@typescript', '@tailwindcss', '@freeCodeCamp', '@mussaWaleed', '@AhmadHassan', '@dan_abramov'],
+            'number': 30
+        },
+        'followers': {
+            'users': ['@reactjs', '@MohamadDewiekat', '@YazanAlsade', '@Node.js', '@typescript', '@tailwindcss', '@freeCodeCamp', '@mussaWaleed', '@AhmadHassan', '@dan_abramov'],
+            'number': 567
+        }
+    },
+    "@YazanAlsade": {
+        'name': 'Yazan Alsade',
+        'description': 'Front-end Developer | UI/UX Designer',
+        'joinedDate': 'January 2017',
+        'webLink': 'yazanalsade.com',
+        'profilePicture': 'https://xsgames.co/randomusers/assets/avatars/male/77.jpg',
+        'profileCover': 'https://example.com/cover/yazan-alsade-cover.png',
+        'postsNumber': 230,
+        'posts': [
+            {
+                'id': 1,
+                'addedBy': '@YazanAlsade',
+                'dateAdded': 'September 18',
+                'content': 'Excited to start working on a new project!',
+                'likes': 180,
+                'comments': 25,
+                'repost': 40,
+                'reached': '80K'
+            },
+        ],
+        'following': {
+            'users': ['@reactjs', '@MohamadDewiekat', '@MohamadKukhun', '@Node.js', '@typescript', '@tailwindcss', '@freeCodeCamp', '@mussaWaleed', '@AhmadHassan', '@dan_abramov'],
+            'number': 55
+        },
+        'followers': {
+            'users': ['@reactjs', '@MohamadDewiekat', '@MohamadKukhun', '@Node.js', '@typescript', '@tailwindcss', '@freeCodeCamp', '@mussaWaleed', '@AhmadHassan', '@dan_abramov'],
+            'number': 789
+        }
+    },
+    "@Node.js": {
+        'name': 'Node.js',
+        'description': 'JavaScript runtime built on Chrome\'s V8 JavaScript engine.',
+        'joinedDate': 'June 2009',
+        'webLink': 'nodejs.org',
+        'profilePicture': 'https://cdn3.iconfinder.com/data/icons/popular-services-brands/512/node-512.png',
+        'profileCover': 'https://example.com/cover/nodejs-cover.png',
+        'postsNumber': 1500,
+        'posts': [
+            {
+                'id': 1,
+                'addedBy': '@Node.js',
+                'dateAdded': 'October 10',
+                'content': 'Node.js is powering the web!',
+                'likes': 3000,
+                'comments': 100,
+                'repost': 500,
+                'reached': '2M'
+            },
+        ],
+        'following': {
+            'users': ['@reactjs', '@MohamadDewiekat', '@MohamadKukhun', '@YazanAlsade', '@typescript', '@tailwindcss', '@freeCodeCamp', '@mussaWaleed', '@AhmadHassan', '@dan_abramov'],
+            'number': 40
+        },
+        'followers': {
+            'users': ['@reactjs', '@MohamadDewiekat', '@MohamadKukhun', '@YazanAlsade', '@typescript', '@tailwindcss', '@freeCodeCamp', '@mussaWaleed', '@AhmadHassan', '@dan_abramov'],
+            'number': 4000000
+        }
+    },
+    "@typescript": {
+        'name': 'TypeScript',
+        'description': 'A typed superset of JavaScript',
+        'joinedDate': 'October 2012',
+        'webLink': 'typescriptlang.org',
+        'profilePicture': 'https://www.typescripttutorial.net/wp-content/uploads/2020/04/favicon.png',
+        'profileCover': 'https://example.com/cover/typescript-cover.png',
+        'postsNumber': 850,
+        'posts': [
+            {
+                'id': 1,
+                'addedBy': '@typescript',
+                'dateAdded': 'November 5',
+                'content': 'TypeScript makes coding safer and more efficient!',
+                'likes': 2500,
+                'comments': 75,
+                'repost': 400,
+                'reached': '1.5M'
+            },
+        ],
+        'following': {
+            'users': ['@reactjs', '@MohamadDewiekat', '@MohamadKukhun', '@YazanAlsade', '@Node.js', '@tailwindcss', '@freeCodeCamp', '@mussaWaleed', '@AhmadHassan', '@dan_abramov'],
+            'number': 60
+        },
+        'followers': {
+            'users': ['@reactjs', '@MohamadDewiekat', '@MohamadKukhun', '@YazanAlsade', '@Node.js', '@tailwindcss', '@freeCodeCamp', '@mussaWaleed', '@AhmadHassan', '@dan_abramov'],
+            'number': 150000
+        }
+    },
+    "@tailwindcss": {
+        'name': 'Tailwind CSS',
+        'description': 'A utility-first CSS framework',
+        'joinedDate': 'April 2017',
+        'webLink': 'tailwindcss.com',
+        'profilePicture': 'https://bourhaouta.gallerycdn.vsassets.io/extensions/bourhaouta/tailwindshades/0.0.5/1592520164095/Microsoft.VisualStudio.Services.Icons.Default',
+        'profileCover': 'https://example.com/cover/tailwindcss-cover.png',
+        'postsNumber': 500,
+        'posts': [
+            {
+                'id': 1,
+                'addedBy': '@tailwindcss',
+                'dateAdded': 'July 15',
+                'content': 'Simplifying web development with Tailwind CSS!',
+                'likes': 1800,
+                'comments': 40,
+                'repost': 300,
+                'reached': '1.2M'
+            },
+        ],
+        'following': {
+            'users': ['@reactjs', '@MohamadDewiekat', '@MohamadKukhun', '@YazanAlsade', '@Node.js', '@typescript', '@freeCodeCamp', '@mussaWaleed', '@AhmadHassan', '@dan_abramov'],
+            'number': 35
+        },
+        'followers': {
+            'users': ['@reactjs', '@MohamadDewiekat', '@MohamadKukhun', '@YazanAlsade', '@Node.js', '@typescript', '@freeCodeCamp', '@mussaWaleed', '@AhmadHassan', '@dan_abramov'],
+            'number': 80000
+        }
+    },
+    "@freeCodeCamp": {
+        'name': 'freeCodeCamp',
+        'description': 'Learn to code for free',
+        'joinedDate': 'December 2014',
+        'webLink': 'freecodecamp.org',
+        'profilePicture': 'https://images.crunchbase.com/image/upload/c_lpad,f_auto,q_auto:eco,dpr_1/ikqra03zdnggljdu5vv0',
+        'profileCover': 'https://example.com/cover/freecodecamp-cover.png',
+        'postsNumber': 3000,
+        'posts': [
+            {
+                'id': 1,
+                'addedBy': '@freeCodeCamp',
+                'dateAdded': 'January 10',
+                'content': 'Coding is the key to the future. Join us on the journey!',
+                'likes': 8000,
+                'comments': 200,
+                'repost': 1000,
+                'reached': '5M'
+            },
+        ],
+        'following': {
+            'users': ['@reactjs', '@MohamadDewiekat', '@MohamadKukhun', '@YazanAlsade', '@Node.js', '@typescript', '@tailwindcss', '@mussaWaleed', '@AhmadHassan', '@dan_abramov'],
+            'number': 75
+        },
+        'followers': {
+            'users': ['@reactjs', '@MohamadDewiekat', '@MohamadKukhun', '@YazanAlsade', '@Node.js', '@typescript', '@tailwindcss', '@mussaWaleed', '@AhmadHassan', '@dan_abramov'],
+            'number': 2500000
+        }
+    },
+    "@mussaWaleed": {
+        'name': 'Mussa Waleed',
+        'description': 'Front-end Developer | Passionate about UI/UX',
+        'joinedDate': 'March 2016',
+        'webLink': 'mussawaleed.com',
+        'profilePicture': 'https://xsgames.co/randomusers/assets/avatars/male/31.jpg',
+        'profileCover': 'https://example.com/cover/mussawaleed-cover.png',
+        'postsNumber': 350,
+        'posts': [
+            {
+                'id': 1,
+                'addedBy': '@mussaWaleed',
+                'dateAdded': 'June 20',
+                'content': 'Just released a new UI component library!',
+                'likes': 1200,
+                'comments': 35,
+                'repost': 75,
+                'reached': '200K'
+            },
+        ],
+        'following': {
+            'users': ['@reactjs', '@MohamadDewiekat', '@MohamadKukhun', '@YazanAlsade', '@Node.js', '@typescript', '@tailwindcss', '@freeCodeCamp', '@AhmadHassan', '@dan_abramov'],
+            'number': 42
+        },
+        'followers': {
+            'users': ['@reactjs', '@MohamadDewiekat', '@MohamadKukhun', '@YazanAlsade', '@Node.js', '@typescript', '@tailwindcss', '@freeCodeCamp', '@AhmadHassan', '@dan_abramov'],
+            'number': 125000
+        }
+    },
+    "@AhmadHassan": {
+        'name': 'Ahmad Hassan',
+        'description': 'Backend Developer | Cloud Enthusiast',
+        'joinedDate': 'August 2017',
+        'webLink': 'ahmadhassan.dev',
+        'profilePicture': 'https://i.pinimg.com/originals/ae/ec/c2/aeecc22a67dac7987a80ac0724658493.jpg',
+        'profileCover': 'https://example.com/cover/ahmadhassan-cover.png',
+        'postsNumber': 420,
+        'posts': [
+            {
+                'id': 1,
+                'addedBy': '@AhmadHassan',
+                'dateAdded': 'September 8',
+                'content': 'Deploying scalable apps on the cloud - A thread.',
+                'likes': 1600,
+                'comments': 50,
+                'repost': 90,
+                'reached': '300K'
+            },
+        ],
+        'following': {
+            'users': ['@reactjs', '@MohamadDewiekat', '@MohamadKukhun', '@YazanAlsade', '@Node.js', '@typescript', '@tailwindcss', '@freeCodeCamp', '@mussaWaleed', '@dan_abramov'],
+            'number': 30
+        },
+        'followers': {
+            'users': ['@reactjs', '@MohamadDewiekat', '@MohamadKukhun', '@YazanAlsade', '@Node.js', '@typescript', '@tailwindcss', '@freeCodeCamp', '@mussaWaleed', '@dan_abramov'],
+            'number': 65000
+        }
+    },
+    "@dan_abramov": {
+        'name': 'Dan Abramov',
+        'description': 'JavaScript Enthusiast | Redux Creator',
+        'joinedDate': 'June 2012',
+        'webLink': 'danabramov.com',
+        'profilePicture': 'https://xsgames.co/randomusers/assets/avatars/male/63.jpg',
+        'profileCover': 'https://example.com/cover/danabramov-cover.png',
+        'postsNumber': 550,
+        'posts': [
+            {
+                'id': 1,
+                'addedBy': '@dan_abramov',
+                'dateAdded': 'November 12',
+                'content': 'Exploring new ways to simplify state management!',
+                'likes': 3000,
+                'comments': 100,
+                'repost': 800,
+                'reached': '2.5M'
+            },
+        ],
+        'following': {
+            'users': ['@reactjs', '@MohamadDewiekat', '@MohamadKukhun', '@YazanAlsade', '@Node.js', '@typescript', '@tailwindcss', '@freeCodeCamp', '@mussaWaleed', '@freeCodeCamp'],
+            'number': 60
+        },
+        'followers': {
+            'users': ['@reactjs', '@MohamadDewiekat', '@MohamadKukhun', '@YazanAlsade', '@Node.js', '@typescript', '@tailwindcss', '@freeCodeCamp', '@mussaWaleed', '@freeCodeCamp'],
+            'number': 500000
+        }
+    },
+};
