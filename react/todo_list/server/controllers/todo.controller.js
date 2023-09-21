101c4b7479f0421d3f1d20cff86adda92ff02372
alirabah93 <110853127+alirabah93@users.noreply.github.com>
2023-09-21 15:53:44 +0300

    apply try and catch method for the back end

--- react/todo_list/server/controllers/todo.controller.js
+++ react/todo_list/server/controllers/todo.controller.js
@@ -1,53 +1,88 @@
 const {Todo} = require('../models/todo.model');
 
-module.exports.createToDo = (request, response) => {
-    const {title, bgColor, cards, isCompleted} = request.body;
-    Todo.create({
-        title,
-        bgColor,
-        cards,
-        isCompleted
-    })
-    .then(todo => response.json(todo))
-    .catch(err => response.status(400).json(err));
-}
+module.exports.createToDo = async (request, response) => {
+    const { title, bgColor, cards, isCompleted } = request.body;
 
-module.exports.getAllTodo = (request, response) => {
-    Todo.find({})
-        .then(todos => response.json(todos))
-        .catch(err => response.status(400).json(err))
-}
-
-module.exports.getTodo = (request, response) => {
-    Todo.findOne({_id: request.params.id})
-        .then(todo => response.json(todo))
-        .catch(err => response.status(400).json(err))
-}
-
-module.exports.updateTodo = (request, response) => {
-    Todo.findOneAndUpdate({_id: request.params.id}, request.body, {new:true})
-        .then(updatedTodo => response.json(updatedTodo))
-        .catch(err => response.status(400).json(err))
-}
-
-
-module.exports.deleteTodo = (request, response) => {
-    Todo.deleteOne({ _id: request.params.id })
-        .then(deleteConfirmation => response.json(deleteConfirmation))
-        .catch(err => response.status(400).json(err))
-}
-
-module.exports.searchTodo = (request, response) => {
-    const query = request.params.query;
-
-    Todo.find({
-        'cards.title': { $regex: `^${query}`, $options: 'i' }
-    })
-        .then(todos => {
-            response.json(todos);
-        })
-        .catch(err => {
-            response.status(400).json(err);
+    try {
+        const todo = await Todo.create({
+            title,
+            bgColor,
+            cards,
+            isCompleted
         });
+        if (!todo) {
+            return response.status(404).json({ message: 'cant create list' });
+        }
+        response.json(todo);
+    } catch (err) {
+        response.status(400).json(err);
+    }
+};
+
+module.exports.getAllTodo = async (request, response) => {
+    try {
+        const todos = await Todo.find({});
+        if (!todos) {
+            return response.status(404).json({ message: 'lists not found' });
+        }
+        response.json(todos);
+    } catch (err) {
+        response.status(400).json(err);
+    }
+};
+
+module.exports.getTodo = async (request, response) => {
+    try {
+        const todo = await Todo.findOne({ _id: request.params.id });
+        if (!todo) {
+            return response.status(404).json({ message: 'Todo not found' });
+        }
+        response.json(todo);
+    } catch (err) {
+        response.status(400).json(err);
+    }
+};
+
+module.exports.updateTodo = async (request, response) => {
+    try {
+        const updatedTodo = await Todo.findOneAndUpdate(
+            { _id: request.params.id },
+            request.body,
+            { new: true }
+        );
+        if (!updatedTodo) {
+            return response.status(404).json({ message: 'Todo not found' });
+        }
+        response.json(updatedTodo);
+    } catch (err) {
+        response.status(400).json(err);
+    }
+};
+
+module.exports.deleteTodo = async (request, response) => {
+    try {
+        const deletedTodo = await Todo.deleteOne({ _id: request.params.id });
+        if(!deletedTodo){
+            return response.status(404).json({ message: 'Todo not found' });
+        }
+        response.json(deletedTodo);
+    } catch (err) {
+        response.status(400).json(err);
+    }
+}
+
+module.exports.searchTodo = async (request, response) => {
+    const query = request.params.query;
+    try {
+        const searchTodo = await Todo.find({
+            'cards.title': { $regex: `^${query}`, $options: 'i' }
+        })
+        if(!searchTodo){
+            return response.status(404).json({ message: 'searched lists not found' });
+        }
+        response.json(searchTodo);
+    } catch (err) {
+        response.status(400).json(err);
+    }
 };
 
