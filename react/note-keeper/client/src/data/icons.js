d3106006c16b8e117cff214b3bacf2eea334d03b
alirabah93 <110853127+alirabah93@users.noreply.github.com>
2023-09-24 21:19:10 +0300

    removing unsed icons

--- react/note-keeper/client/src/data/icons.js
+++ react/note-keeper/client/src/data/icons.js
@@ -1,21 +1,11 @@
-import done from '../icons/done.svg';
-import add from '../icons/add.svg';
 import remove from '../icons/delete.svg';
-import edit from '../icons/edit.svg';
-import more from '../icons/more.svg';
 import search from '../icons/search.svg';
 import close from '../icons/close.svg';
-import expandLess from '../icons/expand_less.svg';
-import expandMore from '../icons/expand_more.svg';
+
+
 
 export const icons = {
-    done,
-    add,
     remove,
-    edit,
-    more,
     search,
     close,
-    expandLess,
-    expandMore
 }
