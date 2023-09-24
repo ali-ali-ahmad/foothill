0f29eae51e9dc4ddfa81a691df02eae83b742d11
alirabah93 <110853127+alirabah93@users.noreply.github.com>
2023-09-24 15:04:45 +0300

    setting up searchBar component

--- react/note-keeper/client/src/components/SearchBar.jsx
+++ react/note-keeper/client/src/components/SearchBar.jsx
@@ -1,9 +1,18 @@
+import { useState } from 'react';
 import styles from './css/SearchBar.module.css';
+import { icons } from '../data/icons';
 
 const  SearchBar = () => {
+    const [searchQuery, setSearchQuery] = useState();
+
     return (
         <div className={styles.container}>
-        
+            <img src={icons.search} alt="Search Icon" />
+            <input 
+                type="text" 
+                placeholder='Search'
+                onChange={(e) => setSearchQuery(e.target.value)}
+            />
         </div>
     );
 }
